498e6ac5e965d5ee73ba16286e776ff2a75ad1c8
Ronno777 <smithryan954@gmail.com>
2026-09-13 16:22:21 -0700

    Reorganized branch for ch4-5 demos

--- DesignPatternLab/Assets/_Project/Scripts/Characters/Bike/Direction.cs
+++
@@ -1,8 +0,0 @@
-namespace DesignPatternLab.Characters.Bike
-{
-    public enum Direction
-    {
-        Left = -1,
-        Right = 1
-    }
-}
--- DesignPatternLab/Assets/_Project/Scripts/Characters/Bike/States/IBikeState.cs
+++
@@ -1,8 +0,0 @@
-namespace DesignPatternLab.Characters.Bike
-{
-    // David Baron, Chapter 5: the contract shared by every bike state.
-    public interface IBikeState
-    {
-        void Handle(BikeController controller);
-    }
-}
